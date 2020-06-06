eb873ee97763c46e056b523e45f7901a8a7b0653
Yu <tuba0927hy@gmail.com>
2020-06-06 18:59:59 +0900

    feat(html): add framework

---
+++ front/src/components/Form.vue
@@ -0,0 +1,16 @@
+<templete>
+  <div class="container">
+    <div class="item">
+      <input type="text" name="title">
+    </div>
+    <div class="item">
+      <input type="text" name="period">
+    </div>
+    <div class="item">
+      <textarea name="detail"></textarea>
+    </div>
+    <div class="item">
+      <button>作成</button>
+    </div>
+  </div>
+</templete>
